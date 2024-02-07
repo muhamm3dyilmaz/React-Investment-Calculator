e5c42603ee830aee7d67bbf47a4a5325f981e4a6
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-07 11:28:14 +0300

    computing values in resuls component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,8 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+
 export default function Results({ input }){
-    console.log(input);
+    const results = calculateInvestmentResults(input);
+    console.log(results);
     return (
         <p>Results......</p>
     );
